2a7e29a7d33f562aaafeea737480c83557b40908
Cord <Cordhernandez@yahoo.com>
2017-04-21 01:07:58 -0700

    Constructs logOutUser() method

--- Chat/Chat/FirebaseAuthorizations.swift
+++ Chat/Chat/FirebaseAuthorizations.swift
@@ -1,3 +1,24 @@
+    func logOutUser() -> Bool {
+        
+        if FIRAuth.auth()?.currentUser != nil {
+            
+            do {
+                
+                try FIRAuth.auth()?.signOut()
+                return true
+            }
+            catch {
+                return true
+            }
+            
+        }
+        
+        return false
+    }
+    
+}
+
+
 //MARK: Firebase Error Handling
 extension FirebaseAuthorization {
     
