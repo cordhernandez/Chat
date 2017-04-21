bd8e80652e24cb9c2ab6b545cf68c1cbe5839b27
Cord <Cordhernandez@yahoo.com>
2017-04-21 01:09:18 -0700

    Constructs FirebaseDataModel structure

---
+++ Chat/Chat/FirebaseDataModel.swift
@@ -0,0 +1,26 @@
+//
+//  FirebaseDataModel.swift
+//  Chat
+//
+//  Created by Cordero Hernandez on 4/20/17.
+//  Copyright © 2017 Cordero. All rights reserved.
+//
+
+import Foundation
+
+struct FirebaseDataModel {
+    
+    static let contacts = "contacts"
+    static let messages = "Messages"
+    static let mediaMessages = "Media_Messages"
+    
+    static let email = "email"
+    static let password = "password"
+    static let data = "data"
+    
+    static let text = "text"
+    static let senderID = "sender_id"
+    static let senderName = "sender_name"
+    static let url = "url"
+    
+}
