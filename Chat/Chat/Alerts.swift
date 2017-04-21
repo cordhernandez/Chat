f851b68f51a74934a7e289759ea21d39f2fb6d76
Cord <Cordhernandez@yahoo.com>
2017-04-21 00:52:15 -0700

    Adds SDWeBImage Cocoa Pod

--- Chat/Chat/Alerts.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Alerts.swift
-//  Chat
-//
-//  Created by Cordero Hernandez on 4/20/17.
-//  Copyright © 2017 Cordero. All rights reserved.
-//
-
-import Foundation
---
+++ Chat/Chat/FirebaseStorageModel.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseStorageModel.swift
+//  Chat
+//
+//  Created by Cordero Hernandez on 4/21/17.
+//  Copyright © 2017 Cordero. All rights reserved.
+//
+
+import Foundation
+
+struct FirebaseStorageModel {
+    
+    static let storageReference = "gs://chat-caaf8.appspot.com"
+    static let imageStorage = "Image_Storage"
+    static let videoStorage = "Video_Storage"
+    
+}
