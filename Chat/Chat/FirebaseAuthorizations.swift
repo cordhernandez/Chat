363070ded55ab243052ffe5657f6ae8feca7338b
Cord <Cordhernandez@yahoo.com>
2017-04-21 01:04:00 -0700

    Constructs callback closure

---
+++ Chat/Chat/FirebaseAuthorizations.swift
@@ -0,0 +1,12 @@
+//
+//  FirebaseAuthorizations.swift
+//  Chat
+//
+//  Created by Cordero Hernandez on 4/20/17.
+//  Copyright © 2017 Cordero. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+typealias loginErrorHandler = (_ errorMessage: String?) -> Void
