75f3dd083e8fa975c06a471b73ad0312389f5f10
Cord <Cordhernandez@yahoo.com>
2017-04-21 17:43:12 -0700

    Constructs Firebase Database References Code

--- Chat/Chat/FirebaseDatabase.swift
+++ Chat/Chat/FirebaseDatabase.swift
@@ -1,3 +1,49 @@
+
+//MARK: Firebase Database References
+extension FirebaseDatabase {
+    
+    var contactsReference: FIRDatabaseReference {
+        return databaseReference.child(FirebaseDataModel.contacts)
+    }
+    
+    var messagesReference: FIRDatabaseReference {
+        return databaseReference.child(FirebaseDataModel.messages)
+    }
+    
+    var mediaMessagesReference: FIRDatabaseReference {
+        return databaseReference.child(FirebaseDataModel.mediaMessages)
+    }
+    
+    var emailReference: FIRDatabaseReference {
+        return databaseReference.child(FirebaseDataModel.email)
+    }
+    
+    var passwordReference: FIRDatabaseReference {
+        return databaseReference.child(FirebaseDataModel.password)
+    }
+    
+    var dataReference: FIRDatabaseReference {
+        return databaseReference.child(FirebaseDataModel.data)
+    }
+    
+    var textReference: FIRDatabaseReference {
+        return databaseReference.child(FirebaseDataModel.text)
+    }
+    
+    var senderIDReference: FIRDatabaseReference {
+        return databaseReference.child(FirebaseDataModel.senderID)
+    }
+    
+    var senderNameReference: FIRDatabaseReference {
+        return databaseReference.child(FirebaseDataModel.senderName)
+    }
+    
+    var urlReference: FIRDatabaseReference {
+        return databaseReference.child(FirebaseDataModel.url)
+    }
+    
+}
+
 //MARK: Firebase Storage References
 extension FirebaseDatabase {
     
