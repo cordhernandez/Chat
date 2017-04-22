5fb93efff4b75bc461ca9a242b8d5960f8893154
Cord <Cordhernandez@yahoo.com>
2017-04-21 17:48:23 -0700

    Constructs tableView(didSelectRowAt) Method

--- Chat/Chat/ContactsTableViewController.swift
+++ Chat/Chat/ContactsTableViewController.swift
@@ -85,6 +85,10 @@ class ContactsTableViewController: UITableViewController {
 //        return cell
 //    }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        goToChatViewController()
+    }
 
     /*
     // Override to support conditional editing of the table view.
