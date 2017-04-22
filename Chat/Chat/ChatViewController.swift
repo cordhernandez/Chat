f67ec14d12e1774e801a6df8c39333e4f4966f8b
Cord <Cordhernandez@yahoo.com>
2017-04-21 17:35:15 -0700

    Imports Frameworks

---
+++ Chat/Chat/ChatViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ChatViewController.swift
+//  Chat
+//
+//  Created by Cordero Hernandez on 4/21/17.
+//  Copyright © 2017 Cordero. All rights reserved.
+//
+
+import AVKit
+import Foundation
+import JSQMessagesViewController
+import MobileCoreServices
+import UIKit
+import SDWebImage
